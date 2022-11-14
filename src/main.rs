f8fefe1ab3ff977923e919e3ba9d0a0fb8fd5e3d
Yuezhch <yuezc1990@gmail.com>
2022-11-14 18:03:10 +0800

    guess

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+    
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("failed to read line");
+
+    println!("You guess: {guess}")    
+}
